59b67ad7d5dca26d988625ddcca1b51eb2dcc6c3
baconmcdouble <sihantang0316@gmail.com>
2023-01-16 15:31:24 -0500

    Added pieces

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ board.style.backgroundColor = '#ADE792';
 board.style.display = 'grid';
 board.style.gridTemplateColumns = 'repeat(8, 30px';
 board.style.gridTemplateRows = 'repeat(8, 30px';
-board.style.gap = '1px';
+board.style.gap = '2px';
 board.style.width = 'fit-content';
+board.style.padding = '5px'
 
 document.body.appendChild(board);
 
@@ -15,4 +16,15 @@ for (let i = 0; i < 64; i++) {
     grid.style.width = '30px';
     grid.setAttribute('id', 'grid' + i);
     board.appendChild(grid);
+
+    grid.addEventListener('click', (() => {
+        const piece = document.createElement('div');
+        piece.style.height = '20px';
+        piece.style.width = '20px';
+        piece.style.borderRadius = '50%';
+        piece.style.border = 'solid';
+        piece.style.margin = '2px';
+
+        grid.appendChild(piece);
+    }))
 }
